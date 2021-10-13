ea405f67aa38527c8a45517cf6a685b9c2c0d6d6
TB056305 <Trupti.Baliga@cerner.com>
2021-10-13 21:17:38 +0530

    groovy

---
+++ 2021/power.groovy
@@ -0,0 +1,12 @@
+/* cerner_2tothe5th_2021 */
+
+/* Code to find the power of a number */
+
+class Power {
+   static void main(String[] args) {
+      double x = 2;
+      double y = 5;
+
+      System.out.printf("%.3f to the power %.3f is %.3f%n", x, y, Math.pow(x, y));
+   }
+}
