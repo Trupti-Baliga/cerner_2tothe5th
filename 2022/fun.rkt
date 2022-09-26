560e155a0fe4663b109b61d3d2113ed9e7b2373d
TB056305 <Trupti.Baliga@cerner.com>
2022-09-26 21:53:32 +0530

    Day12: Racket

---
+++ 2022/fun.rkt
@@ -0,0 +1,17 @@
+; cerner_2tothe5th_2022
+#lang racket/base
+(display "2 to the power 5 is ")
+(expt 2 5)
+
+(display "3 plus 2 is ")
+(+ 3 2)
+
+
+(display "Is 5 a number? ")
+(number? 5)
+
+(display "Is 2+3i a number? ")
+(number? 2+3i)
+
+(display "Whats the product of complex numbers? ")
+(* 2+3i 3+2i)
