70d2002771c50a1f67fbd93d4fb7a11fd95fec02
TB056305 <Trupti.Baliga@cerner.com>
2022-10-12 22:12:43 +0530

    Day28: R

---
+++ 2022/random.r
@@ -0,0 +1,29 @@
+# cerner_2tothe5th_2022
+
+# prints sum of numbers between 0 to 9.
+cat("sum of numbers between 0 to 9:");
+sum(0:9)
+
+# print all alphabet in upper case
+cat("ALPHABETS - ");
+LETTERS
+
+# print alphabet in upper and lower case
+cat("ALPHAbets - ");
+append(LETTERS[1:13],letters[14:26])
+
+# prints a list from 1 to 4 in steps of 0.5
+seq(from=1,to=4,by=.5)
+
+# prints out month abbrevations
+cat("Months of the calendar: ")
+month.abb
+
+# sequence addition - Add 5 to all numbers in list
+c(1,2,3) + 5
+
+# generate a random number between 0 and 1
+runif(1)
+
+# generate 4 random number between 0 and 1
+runif(4)
