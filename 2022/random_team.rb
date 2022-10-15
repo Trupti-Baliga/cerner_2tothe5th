7a3474dec90662f28642602b1a944099b7e92457
TB056305 <Trupti.Baliga@cerner.com>
2022-10-15 22:14:18 +0530

    Day31: Ruby

---
+++ 2022/random_team.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+# cerner_2tothe5th_2022
+# Randomly select an entry from array.
+# ruby random_team.rb
+# try it online here - https://tio.run/#ruby
+team_names = ['gryffindor', 'old monks', 'gladiators', 'code busters', 'dynamo', 'afk', 'out of office', 'data stormers', 'REScue']
+puts "The team which has been randomly chosen is - #{team_names.sample}"
